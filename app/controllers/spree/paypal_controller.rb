5eebcc16019c34aa67584473548fd1dcfb62480e
David Dawson <david.dawson@gmail.com>
2014-04-24 14:11:08 +0100

    modify the flashe message from: 	commerce_tracking to 	order_completed

--- app/controllers/spree/paypal_controller.rb
+++ app/controllers/spree/paypal_controller.rb
@@ -55,7 +55,7 @@ module Spree
       order.next
       if order.complete?
         flash.notice = Spree.t(:order_processed_successfully)
-        flash[:commerce_tracking] = "nothing special"
+        flash[:order_completed] = true
         redirect_to completion_route(order)
       else
         redirect_to checkout_state_path(order.state)
